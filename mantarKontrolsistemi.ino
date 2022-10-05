ddcdc68a2c784d238434d89146036a85bb943fdf
ibmtalJava <114569460+ibmtalJava@users.noreply.github.com>
2022-10-05 15:47:07 +0300

    last state

--- mantarKontrolsistemi.ino
+++ mantarKontrolsistemi.ino
@@ -7,4 +7,5 @@ void setup() {
 
 void loop() {
  Serial.println(mantarSensor.temp());
+ mantarSensor.attach();
 }
--- mantarSensor.h
+++ mantarSensor.h
@@ -5,6 +5,8 @@ class MantarSensor{
     int tempUpPin=0;
     int tempSensorPin=0;
     double tempValue=0;
+    double minTemp=22.2;
+    double maxTemp=26.8;
     MantarSensor(int tsp,int tdp,int tnp,int tup){
         tempSensorPin=tsp;
         tempDownPin=tdp;
@@ -18,7 +20,18 @@ class MantarSensor{
     }
     double temp(){
        tempValue=analogRead(tempSensorPin);
+       tempValue=tempValue/6.82;
+       tempValue=tempValue-50;
        return tempValue;
+    }
+    void attach(){
+         temp();
+         if(tempValue<minTemp) digitalWrite(tempDownPin,HIGH); else  digitalWrite(tempDownPin,LOW);
+         if(tempValue>maxTemp) digitalWrite(tempUpPin,HIGH); else  digitalWrite(tempUpPin,LOW);
+         if(tempValue>=minTemp&&tempValue<=maxTemp)
+          digitalWrite(tempNormalPin,HIGH); 
+         else
+          digitalWrite(tempNormalPin,LOW);
     }
   private:
 };
